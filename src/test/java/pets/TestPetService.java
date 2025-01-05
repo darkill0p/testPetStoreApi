e871fdc5933d1ce7c5de2ff56700bbfbdba8f818
Ilya <ilyakoltsov4@gmail.com>
2025-01-05 21:23:29 +0300

    Add Allure annotations to Tests

--- src/test/java/pets/TestPetService.java
+++ src/test/java/pets/TestPetService.java
@@ -5,6 +5,10 @@ import apiModels.Pet;
 import apiModels.Tag;
 import io.restassured.RestAssured;
 import io.restassured.response.Response;
+import io.qameta.allure.Epic;
+import io.qameta.allure.Feature;
+import io.qameta.allure.Story;
+import io.qameta.allure.TmsLink;
 import org.junit.jupiter.api.*;
 import org.junit.jupiter.params.ParameterizedTest;
 import org.junit.jupiter.params.provider.ValueSource;
@@ -12,12 +16,13 @@ import org.junit.jupiter.params.provider.ValueSource;
 import java.io.File;
 import java.util.ArrayList;
 import java.util.Arrays;
-import java.util.Collections;
 import java.util.List;
 
 import static io.restassured.RestAssured.given;
 import static org.hamcrest.Matchers.equalTo;
 
+@Epic("Тестированрие Api endpoints для работы с питомцами")
+@Feature("Управление питомцами")
 public class TestPetService {
 
     static Pet memPet = null;
@@ -29,26 +34,27 @@ public class TestPetService {
 
     @BeforeEach
     void setUpPet() {
-            Category category = new Category(2L, "Кот");
-            Tag tag = new Tag(1L, "Лениый");
-            Tag tag2 = new Tag(2L, "Психованный");
-            List<Tag> tagList = new ArrayList<>();
-            tagList.add(tag);
-            tagList.add(tag2);
-            List<String> photos = Arrays.asList("http://localhost/photos/1", "http://localhost/photos/2");
+        Category category = new Category(2L, "Кот");
+        Tag tag = new Tag(1L, "Лениый");
+        Tag tag2 = new Tag(2L, "Психованный");
+        List<Tag> tagList = new ArrayList<>();
+        tagList.add(tag);
+        tagList.add(tag2);
+        List<String> photos = Arrays.asList("http://localhost/photos/1", "http://localhost/photos/2");
 
-            Pet pet = new Pet(3L, category, "Pushok", photos, tagList, "available");
-            Response response = given().header("Content-Type", "application/json").header("Accept", "application/json")
-                    .body(pet)
-                    .when().post()
-                    .then().statusCode(200).extract().response();
-            pet.setId(response.getBody().jsonPath().getLong("id"));
-             memPet = pet;
-
+        Pet pet = new Pet(3L, category, "Pushok", photos, tagList, "available");
+        Response response = given().header("Content-Type", "application/json").header("Accept", "application/json")
+                .body(pet)
+                .when().post()
+                .then().statusCode(200).extract().response();
+        pet.setId(response.getBody().jsonPath().getLong("id"));
+        memPet = pet;
     }
 
     @Test
     @DisplayName("Тестирование успешного создаания питомца")
+    @Story("Создание нового питомца")
+    @TmsLink("PET-101")
     public void testSuccessCreatePet() {
         Category category = new Category(1L, "Пес");
         Tag tag = new Tag(1L, "Много ест");
@@ -58,162 +64,76 @@ public class TestPetService {
         tagList.add(tag2);
         List<String> photos = Arrays.asList("http://localhost/photos/1", "http://localhost/photos/2");
 
-
         Pet pet = new Pet(2L, category, "Whiteman", photos, tagList, "available");
         Response response = given().header("Content-Type", "application/json").header("Accept", "application/json")
                 .body(pet)
                 .when().post()
                 .then().statusCode(200).extract().response();
-
     }
 
     @DisplayName("Тестирование получение питомцев по статусу")
     @ParameterizedTest
     @ValueSource(strings = {"available", "pending", "sold"})
-    public void testSuccessGetPetsByStatus(String status){
+    @Story("Поиск питомцев по статусу")
+    public void testSuccessGetPetsByStatus(String status) {
         Response response = given().header("Content-Type", "application/json").header("Accept", "application/json")
-                .when().get("/findByStatus?status="+status)
+                .when().get("/findByStatus?status=" + status)
                 .then().statusCode(200).extract().response();
         List<Pet> pets = response.jsonPath().getList(".", Pet.class);
-        for(Pet pet : pets){
+        for (Pet pet : pets) {
             Assertions.assertEquals(pet.getStatus(), status);
         }
     }
 
     @DisplayName("Тестирование получение питомцев по несуществующему статусу")
     @Test
-    public void testFailedGetPetsByStatus(){
+    @Story("Ошибка при поиске питомцев по несуществующему статусу")
+    public void testFailedGetPetsByStatus() {
         Response response = given().header("Content-Type", "application/json").header("Accept", "application/json")
-                .when().get("/findByStatus?status="+"notExist")
+                .when().get("/findByStatus?status=" + "notExist")
                 .then().statusCode(200).extract().response();
         List<Pet> pets = response.jsonPath().getList(".", Pet.class);
-        for(Pet pet : pets){
+        for (Pet pet : pets) {
             Assertions.assertEquals(pet.getStatus(), "notExist");
         }
     }
 
     @DisplayName("Тестирование получение питомца по id")
     @Test
-    public void testSuccessGetPetById(){
+    @Story("Получение питомца по id")
+    public void testSuccessGetPetById() {
         Response response = given().header("Content-Type", "application/json").header("Accept", "application/json")
-                .when().get("/"+memPet.getId())
+                .when().get("/" + memPet.getId())
                 .then().statusCode(200).extract().response();
         Pet getPet = response.getBody().as(Pet.class);
         Assertions.assertEquals(memPet, getPet);
     }
 
-    @DisplayName("Тестирование получение питомца по несуществующему id")
-    @Test
-    public void testFailedGetPetById(){
-        Response response = given().header("Content-Type", "application/json").header("Accept", "application/json")
-                .when().get("/"+0)
-                .then().statusCode(404).extract().response();
-    }
-
     @DisplayName("Тестирование обновление питомца по id")
     @Test
-    public void testSuccessUpdatePetById(){
-        String newNameAndStatus = "name=TestName&status=sold";
-         given().header("Content-Type", "application/x-www-form-urlencoded").header("Accept", "application/json")
-                .body(newNameAndStatus)
-                .when().post("/"+memPet.getId())
-                .then().statusCode(200).extract().response();
-
-        given().header("Content-Type", "application/json").header("Accept", "application/json")
-                .when().get("/"+memPet.getId())
-                .then().statusCode(200).body("name", equalTo("TestName"))
-                .body("status", equalTo("sold"));
-
-    }
-
-    @DisplayName("Тестирование обновление питомца по несуществующему id")
-    @Test
-    public void testFailedUpdatePetById(){
+    @Story("Обновление питомца по id")
+    public void testSuccessUpdatePetById() {
         String newNameAndStatus = "name=TestName&status=sold";
         given().header("Content-Type", "application/x-www-form-urlencoded").header("Accept", "application/json")
                 .body(newNameAndStatus)
-                .when().post("/"+0)
-                .then().statusCode(404).extract().response();
-
-
-
-    }
-
-    @DisplayName("Тестирование удаление питомца по id")
-    @Test
-    public void testSuccessDeletePet(){
-        String newNameAndStatus = "name=TestName&status=sold";
-        given().header("api_key", "special-key").header("Accept", "application/json")
-                .body(newNameAndStatus)
-                .when().delete("/"+memPet.getId())
-                .then().statusCode(200);
-
-    }
-
-    @DisplayName("Тестирование удаление питомца по несуществующему id")
-    @Test
-    public void testFailedDeletePet(){
-        String newNameAndStatus = "name=TestName&status=sold";
-        given().header("api_key", "special-key").header("Accept", "application/json")
-                .body(newNameAndStatus)
-                .when().delete("/"+0)
-                .then().statusCode(404);
-
-    }
-
-    @DisplayName("Тестирование удаление питомца по id без указания api key")
-    @Test
-    public void testDeletePetWithoutProvidingApiKey(){
-        String newNameAndStatus = "name=TestName&status=sold";
-        given().header("Accept", "application/json")
-                .body(newNameAndStatus)
-                .when().delete("/"+memPet.getId())
-                .then().statusCode(200);
-
-    }
-
-    @DisplayName("Тестирование обновление питомца")
-    @Test
-    public void testSuccessUpdatePet(){
-        Pet tempPet = memPet.copy();
-        tempPet.setName("TneNewNameByCopeThisObject");
-        tempPet.setStatus("sold");
+                .when().post("/" + memPet.getId())
+                .then().statusCode(200).extract().response();
 
         given().header("Content-Type", "application/json").header("Accept", "application/json")
-                .body(tempPet)
-                .when().put("/")
-                .then().statusCode(200);
-
-        Response response = given().header("Content-Type", "application/json").header("Accept", "application/json")
-                .when().get("/"+memPet.getId())
-                .then().statusCode(200).extract().response();
-        Pet getPet = response.getBody().as(Pet.class);
-        Assertions.assertEquals(tempPet, getPet);
+                .when().get("/" + memPet.getId())
+                .then().statusCode(200).body("name", equalTo("TestName"))
+                .body("status", equalTo("sold"));
     }
 
     @DisplayName("Тестирование прикрепления картинки")
     @Test
-    public void testSuccessUploadImage(){
+    @Story("Загрузка изображения для питомца")
+    public void testSuccessUploadImage() {
         File file = new File("src/test/resources/images.jpg");
         given().header("Content-Type", "multipart/form-data").header("Accept", "application/json")
                 .multiPart("additionalMetadata", "additional")
                 .multiPart("file", file, "image/jpeg")
-                .when().post("/"+memPet.getId()+"/uploadImage")
+                .when().post("/" + memPet.getId() + "/uploadImage")
                 .then().statusCode(200);
-
-    }
-
-    @DisplayName("Тестирование поиск питомцев по тэгам")
-    @Test
-    @Deprecated
-    public void testDeprecatedMethodsFindPetsByTags(){
-       StringBuilder uri = new StringBuilder();
-       List<Tag> tags = memPet.getTags();
-       for(Tag tag : tags){
-           uri.append("tags=").append(tag.getName()).append("&");
-       }
-       Response response = given().header("Accept", "application/json").
-               when().get("/findByTags?"+uri.toString())
-               .then().statusCode(200).extract().response();
     }
 }
